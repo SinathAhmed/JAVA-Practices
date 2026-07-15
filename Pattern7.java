15526d284d8973852f8b7fa5192bfe71a024dded
Sinath Ahmed Fahad <136341585+SinathAhmed@users.noreply.github.com>
2026-07-15 21:31:32 +0600

    Add pattern printing logic in Pattern7.java
    Implement a pattern printing program that prints numbers in a pyramid format.

---
+++ Pattern7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.LinkedList;
+public class first {
+    public static void main(String[] args){
+      System.out.println("Hello World \n");
+
+    /*  1
+      2   2
+    3   3   3
+  4   4   4   4 */
+      
+    for (int i = 1; i <= a; i++) {
+            for (int k = 1; k < a-i; k++) {
+                System.out.print(" ");
+            }
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(i+" ");
+                }
+                System.out.print("\n");
+            }
+    }
+}
